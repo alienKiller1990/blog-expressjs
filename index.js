fe3cd21b82706b3296da874b10fd5af9cdc8239f
Pavel Che <alien.killer3@bk.ru>
2021-10-17 16:12:24 +0300

    test gitignore

--- index.js
+++ index.js
@@ -48,4 +48,4 @@ mongoose.connect('mongodb://localhost:27017/test');
 const Cat = mongoose.model('Cat', { name: String });
 
 const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
+kitty.save().then(() => console.log('meow!'));
