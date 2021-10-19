734c5a4d175bd99ac0d3ce2969a7392a37dbbdc8
Pavel Che <alien.killer3@bk.ru>
2021-10-19 11:14:11 +0300

    fixed bug with launching mongoose

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+
+import mongoose from 'mongoose';
+import PostModel from './models/Post.js';
+
+mongoose.connect('mongodb://localhost/blog');
+
+const post = new PostModel({
+    title: 'Новая запись',
+    text: 'Hello world'
+});
+
+post.save().then(() => {
+    console.log('Ok!');
+});
