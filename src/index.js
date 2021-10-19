a485b1a08dca759d60a92b4c5249243f7f6295ea
Pavel Che <alien.killer3@bk.ru>
2021-10-19 11:59:11 +0300

    set up sending post and get requests

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 
 import mongoose from 'mongoose';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
 import PostModel from './models/Post.js';
 
+const app = express();
 mongoose.connect('mongodb://localhost/blog');
 
-const post = new PostModel({
-    title: 'Новая запись',
-    text: 'Hello world'
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.post('/posts', (req, res) => {
+
+    const data = req.body;
+    
+    const post = new PostModel({
+        title: data.title,
+        text: data.text
+    });
+    
+    post.save().then(() => {
+        res.send({status: 'ok'})
+    });
 });
 
-post.save().then(() => {
-    console.log('Ok!!')
+app.get('/posts', (req, res) => {
+    PostModel.find().then((err, posts) => {
+        if (err) {
+            return res.send(err);
+        }
+        res.json(posts)
+    });
+});
+
+app.listen(3333, () => {
+    console.log('server started!')
 })
 
