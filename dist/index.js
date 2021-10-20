e87ffe13b02eb253766c8e5628feb3f31e4a5393
Pavel Che <alien.killer3@bk.ru>
2021-10-20 11:50:55 +0300

    create postcontroller and put cod of requests there

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import PostModel from './models/Post.js';
+// import PostModel from './models/Post.js';
+import PostController from './controllers/PostController.js';
+const Post = new PostController();
 
 const app = express();
 mongoose.connect('mongodb://localhost/blog');
@@ -11,50 +13,14 @@ mongoose.connect('mongodb://localhost/blog');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/posts', (req, res) => {
+app.post('/posts', Post.index());
 
-    const data = req.body;
+app.get('/posts', Post.create());
+app.get('/posts', Post.read());
 
-    const post = new PostModel({
-        title: data.title,
-        text: data.text
-    });
+app.delete('/posts/:id', Post.read());
 
-    post.save().then(() => {
-        res.send({ status: 'ok' });
-    });
-});
-
-app.get('/posts', (req, res) => {
-    PostModel.find().then((err, posts) => {
-        if (err) {
-            return res.send(err);
-        }
-        res.json(posts);
-    });
-});
-
-app.delete('/posts/:id', (req, res) => {
-    PostModel.remove({
-        _id: req.params.id
-    }).then(post => {
-
-        if (post) {
-            res.json({ status: 'deleted' });
-        } else {
-            res.json({ status: 'error' });
-        }
-    });
-});
-
-app.put('/posts/:id', (req, res) => {
-    PostModel.findByIdAndUpdate(req.params.id, { $set: req.body }, err => {
-        if (err) {
-            return res.send(err);
-        }
-        res.json({ status: 'updated' });
-    });
-});
+app.put('/posts/:id', Post.update());
 
 app.listen(3333, () => {
     console.log('server started!');
